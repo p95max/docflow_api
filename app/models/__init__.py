f38204f8a33ce5e4f0139e28379ee6eabf99135f
Maksym Petrykin <maxpetrikin@gmail.com>
2026-07-13 12:51:42 +0200

    Register backup model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,15 @@
 from app.models.audit_log import AuditLog
+from app.models.backup_job import BackupJob
 from app.models.document import Document
 from app.models.openai_usage_log import OpenAIUsageLog
 from app.models.processing_job import ProcessingJob
 from app.models.user import User
 
-__all__ = ["AuditLog", "Document", "OpenAIUsageLog", "ProcessingJob", "User"]
+__all__ = [
+    "AuditLog",
+    "BackupJob",
+    "Document",
+    "OpenAIUsageLog",
+    "ProcessingJob",
+    "User",
+]
